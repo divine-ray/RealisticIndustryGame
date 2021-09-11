b0d7b7de967b6fb6fada6a2b289c5bef854f262a
CreepTV <lixernispunkt2@gmx.net>
2021-09-12 00:18:00 +0200

    removed all the junky assets
    only those not needed plz dont break stuff

--- Assets/3rd party asset/TwoPiGrid/GridSystem/Runtime/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("TwoPiGrid.Tests")]
-[assembly: InternalsVisibleTo("TwoPiGrid.Editor")]
-[assembly: InternalsVisibleTo("TwoPiGrid.Visualization")]
-[assembly: InternalsVisibleTo("TwoPiGrid.Visualization.Editor")]

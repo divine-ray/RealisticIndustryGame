d2fc08ca1743732e22e71f660c905b2572782aa2
CreepTV <lixernispunkt2@gmx.net>
2021-09-11 11:50:36 +0200

    uh idk, more copyage of formulas

---
+++ Assets/scripts/Testing.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(Mathf.Exp(6));
+        print(Mathf.Pow(6, 2));
+        print(Mathf.Pow(10, 3));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/scripts/tempsim/tempSIm.cs
+++ Assets/scripts/tempsim/tempSIm.cs
@@ -1,31 +1,49 @@
+using UnityEngine;
 
 public class TempSim
 {
+    public double temperatureOfRadiator; //T
+    public double temperatureOfSurroundings; //Tc
 
-    private double heat_added;
-    private double specific_heat;
-    private double mass;
-    private double delta_T;
+    private double calcVar1;
+    private double calcVar2;
+
+    public double heat_added;      //Q
+    private double specific_heat; //c
+    public double mass;          //m
+    private double delta_T;         
     private double tFinal;
     private double tInitial;
-    public int time;
-    public double thermalConductivity;
+
+    public int time;            //t
+    public double thermalConductivity;  //k
     private double Thot;
     private double Tcold;
 
+    public double netRadiatedPower; //P
+    private double radiatingArea; //A
+    private double stefanConstant = 0.0000000567; //funny o
+    public double emissivity; //e
+    
     private void SpecificHeat()
     {
         delta_T = tFinal - tInitial;
         heat_added = specific_heat * mass * delta_T;
     }
 
-    private void heatTransfer()
+    private void HeatTransfer()
     {
         delta_T = Thot - Tcold;
         heat_added = thermalConductivity * delta_T / mass;
     }
 
-
+    private void HeatRadiation()
+    {
+      
+        delta_T = Mathf.Pow((float)temperatureOfRadiator, 4) - Mathf.Pow((float)temperatureOfSurroundings, 4);
+        netRadiatedPower = emissivity * stefanConstant * radiatingArea * delta_T;
+        
+    }
 
 
 
